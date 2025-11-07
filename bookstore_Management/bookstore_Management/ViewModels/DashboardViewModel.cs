4a8e90a03c27db2c80f108b497831aaa950eb218
thanhtran <hongthanh3aqn@gmail.com>
2025-11-08 01:48:57 +0700

    update folder tree

--- bookstore_Management/bookstore_Management/ViewModels/DashboardViewModel.cs
+++ bookstore_Management/bookstore_Management/ViewModels/DashboardViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace bookstore_Management.ViewModels
 {
-    internal class OrderViewModel
+    internal class DashboardViewModel
     {
     }
 }
--- bookstore_Management/bookstore_Management/ViewModels/LoginViewModel.cs
+++ bookstore_Management/bookstore_Management/ViewModels/LoginViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace bookstore_Management.ViewModels
 {
-    internal class BaseViewModel
+    internal class LoginViewModel
     {
     }
 }
