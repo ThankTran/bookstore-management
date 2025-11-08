8dbe0892a1bb55dae38dd5c4d1942eb0ebdc45b6
thanhtran <hongthanh3aqn@gmail.com>
2025-11-08 12:58:18 +0700

    update folder tree

---
+++ bookstore_Management/bookstore_Management/Core/Enums/PaymentType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bookstore_Management.Core.Enums
+{
+    internal class PaymentType
+    {
+    }
+}
---
+++ bookstore_Management/bookstore_Management/Presentation/Views/Inventory/ImportBooksView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace bookstore_Management.Presentation.Views.Inventory
+{
+    /// <summary>
+    /// Interaction logic for ImportBooksView.xaml
+    /// </summary>
+    public partial class ImportBooksView : Window
+    {
+        public ImportBooksView()
+        {
+            InitializeComponent();
+        }
+    }
+}
