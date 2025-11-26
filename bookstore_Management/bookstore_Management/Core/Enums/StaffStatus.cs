96c30c00dad5cbe5747589264f0a19ab432ffd94
hienpham0344 <hienpham0344@gmail.com>
2025-11-26 22:20:48 +0700

    add staff status - type enum

---
+++ bookstore_Management/bookstore_Management/Core/Enums/StaffStatus.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bookstore_Management.Core.Enums
+{
+    public enum StaffStatus
+    {
+        [Display(Name = "Vẫn đang làm việc")]
+        Working = 0,
+        
+        [Display(Name = "Đã nghỉ việc")]
+        Quit = 1,
+        
+        [Display(Name = "Trong kì nghỉ phép")]
+        Rest = 2
+    }
+}
