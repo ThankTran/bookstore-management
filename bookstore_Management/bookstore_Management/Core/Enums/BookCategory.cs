e22619479a725417c1638c6200ae25eebc7a5e39
hienpham0344 <hienpham0344@gmail.com>
2025-11-26 22:18:25 +0700

    add book category - type enum

---
+++ bookstore_Management/bookstore_Management/Core/Enums/BookCategory.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bookstore_Management.Core.Enums
+{
+    public enum BookCategory
+    {
+        [Display(Name = "Sách thiếu nhi")]
+        Children = 1,
+        
+        [Display(Name = "Văn học")]
+        Literature = 2,
+        
+        [Display(Name = "Tâm lý")]
+        Psychology = 3,
+        
+        [Display(Name = "Kinh tế")]
+        Economics = 4,
+        
+        [Display(Name = "Sách giáo khoa")]
+        Textbook = 5,
+        
+        [Display(Name = "Tiểu sử")]
+        Biography = 6,
+        
+        [Display(Name = "Sách ngoại ngữ")]
+        ForeignLanguage = 7
+    }
+}
