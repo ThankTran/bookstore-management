71e8dd6e17c4f59d736d61f5e0c9aa632d7ab614
hienpham0344 <hienpham0344@gmail.com>
2025-11-26 22:22:22 +0700

    add member tier - type enum

---
+++ bookstore_Management/bookstore_Management/Core/Enums/MemberTier.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bookstore_Management.Core.Enums
+{
+    public enum MemberTier
+    {
+        [Display(Name = "Vãng lai")]
+        WalkIn = 0,
+        
+        [Display(Name = "Thành viên đồng")]
+        Bronze = 1,
+        
+        [Display(Name = "Thành viên bạc")]
+        Silver = 2,
+        
+        [Display(Name = "Thành viên vàng")]
+        Gold = 3,
+        
+        [Display(Name = "Thành viên kim cương")]
+        Diamond = 4
+    }
+}
