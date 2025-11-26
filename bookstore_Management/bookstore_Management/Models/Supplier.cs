be6aefe3e0cdc7d4142160a7e560cddb2a76fd87
hienpham0344 <hienpham0344@gmail.com>
2025-11-26 22:11:14 +0700

    add supplier model

--- bookstore_Management/bookstore_Management/Models/Supplier.cs
+++ bookstore_Management/bookstore_Management/Models/Supplier.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace bookstore_Management.Models
 {
+    /// <summary>
+    /// Thông tin của các nhà cung cấp cần quản lý
+    /// </summary>
     internal class Supplier
     {
+        // mã nhà cung cấp - khóa chính
+        [Column("id")]
+        [StringLength(6)]
+        public string Id { get; set; }
+
+        // tên nhà cung cấp
+        [Required]
+        [Column("name")]
+        [StringLength(50)]
+        public string Name { get; set; } 
+        
+        // điện thoại nhà cung cấp
+        [Required]
+        [Column("phone")]
+        [StringLength(30)]
+        public string Phone { get; set; }
+        
+        // địa chỉ nhà cung cấp - có thể null
+        [Column("address")]
+        public string Address { get; set; }
+        
+        // email nhà cung cấp - có thể null
+        [Column("email")]
+        public string Email { get; set; }
+        
+        // navigation properties
+        public virtual ICollection<Book> Books { get; set; }
     }
 }
