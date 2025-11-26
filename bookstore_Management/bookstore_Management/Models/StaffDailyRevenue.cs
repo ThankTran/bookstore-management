cfcd4334b30a13e1c38728d207a7901e282ace14
hienpham0344 <hienpham0344@gmail.com>
2025-11-26 22:12:47 +0700

    git add staff dailuy revenue

---
+++ bookstore_Management/bookstore_Management/Models/StaffDailyRevenue.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace bookstore_Management.Models
+{
+    /// <summary>
+    /// Thông tin cần quản lí của nhân viên - doanh thu trên tháng
+    /// </summary>
+    public class StaffDailyRevenue
+    {
+        // mã nhân viên - khóa chính - khóa ngoại
+        [Required]
+        [StringLength(6)]
+        [Column("employee_id")]
+        public string EmployeeId { get; set; }
+
+        // ngày làm - khóa chính 
+        [Required]
+        [Column("day")]
+        [DataType(DataType.Date)]
+        public DateTime Day { get; set; }
+
+        // doanh thu
+        [Required]
+        [Column("revenue")]
+        [DataType(DataType.Currency)]
+        public decimal Revenue { get; set; }
+        
+        // navigation properties
+        public virtual Staff Staff { get; set; } // 1 doanh thu chỉ có 1 nhân viên
+    }
+}
