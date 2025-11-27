ba85ef15046e749959c76929b83e6d849901b240
my <24521092@gm.uit.edu.vn>
2025-11-27 15:13:43 +0700

    update dialog books/customers/employees

---
+++ bookstore_Management/bookstore_Management/Presentation/Views/Dialogs/Employees/InputEmployeesDialog.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace bookstore_Management.Presentation.Views.Dialogs.Employees
+{
+    /// <summary>
+    /// Interaction logic for InputEmployeesDialog.xaml
+    /// </summary>
+    public partial class InputEmployeesDialog : Window
+    {
+        public InputEmployeesDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
