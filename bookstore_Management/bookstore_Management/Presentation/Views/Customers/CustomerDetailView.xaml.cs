9aba06703e5bf2ef509bc5115d1dab7a454098e5
thanhtran <hongthanh3aqn@gmail.com>
2025-11-29 15:41:15 +0700

    update customer page

--- bookstore_Management/bookstore_Management/Presentation/Views/Customers/CustomerDetailView.xaml.cs
+++ bookstore_Management/bookstore_Management/Presentation/Views/Customers/CustomerDetailView.xaml.cs
@@ -1,41 +1,83 @@
 ﻿using System;
-using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace bookstore_Management.Views.Customers
 {
-    /// <summary>
-    /// Interaction logic for CustomerDetailView.xaml
-    /// </summary>
     public partial class CustomerDetailView : UserControl
     {
-        public ObservableCollection<History> Histories { get; set; }
+        public event EventHandler ReturnToList;
 
+        #region Constructors
+
+        // Khởi tạo màn hình chi tiết khách hàng
         public CustomerDetailView()
         {
             InitializeComponent();
-            LoadSampleData();          // gọi để đổ dữ liệu demo
         }
 
-        private void LoadSampleData()
+        #endregion
+
+        #region Public methods
+
+        // Nạp dữ liệu chi tiết cho một khách hàng
+        public void LoadCustomer(Customer customer)
         {
-            Histories = new ObservableCollection<History>
+            if (customer == null) return;
+
+            txtMaKH.Text = customer.MaKH;
+            txtTenKH.Text = customer.TenKH;
+            txtSDT.Text = customer.SDT;
+            txtHangThanhVien.Text = customer.HangThanhVien;
+            txtDoanhThu.Text = string.Format("{0:N0} VNĐ", customer.DoanhThu);
+
+            txtDiaChi.Text = "123 Nguyễn Văn Linh, Quận 7, TP.HCM";
+            txtEmail.Text = customer.MaKH.ToLower() + "@email.com";
+
+            txtDiemTichLuy.Text = "1,500 điểm";
+
+            LoadPurchaseHistory();
+        }
+
+        #endregion
+
+        #region Private methods
+
+        // Tải dữ liệu lịch sử mua hàng mẫu cho khách hàng hiện tại
+        private void LoadPurchaseHistory()
+        {
+            var histories = new List<PurchaseHistory>
             {
-                new History { STT = 1, MaHD = "HD100", TongTien = "100.000.000", NgayMuaHang = "27/06/2006", GhiChu = "NULL" },
-                new History { STT = 2, MaHD = "HD101", TongTien = "50.000.000",  NgayMuaHang = "10/07/2006", GhiChu = "NULL" },
-                new History { STT = 3, MaHD = "HD102", TongTien = "75.000.000",  NgayMuaHang = "20/08/2006", GhiChu = "Khách VIP" },
+                new PurchaseHistory { STT = 1, MaHD = "HD001", TongTien = "500,000 VNĐ", NgayMuaHang = "01/11/2024", GhiChu = "Mua sách văn học" },
+                new PurchaseHistory { STT = 2, MaHD = "HD002", TongTien = "750,000 VNĐ", NgayMuaHang = "10/11/2024", GhiChu = "Mua sách giáo khoa" },
+                new PurchaseHistory { STT = 3, MaHD = "HD003", TongTien = "1,200,000 VNĐ", NgayMuaHang = "20/11/2024", GhiChu = "Mua sách tham khảo" },
+                new PurchaseHistory { STT = 4, MaHD = "HD004", TongTien = "300,000 VNĐ", NgayMuaHang = "25/11/2024", GhiChu = "Mua văn phòng phẩm" }
             };
 
-            dgHistories.ItemsSource = Histories;
+            dgHistories.ItemsSource = histories;
         }
 
-        public class History
+        // Xử lý khi người dùng nhấn nút quay lại danh sách khách hàng
+        private void btnReturn_Click(object sender, RoutedEventArgs e)
         {
-            public int STT { get; set; }
-            public string MaHD { get; set; }
-            public string TongTien { get; set; }
-            public string NgayMuaHang { get; set; }
-            public string GhiChu { get; set; }
+            ReturnToList?.Invoke(this, EventArgs.Empty);
         }
+
+        #endregion
     }
+
+    #region Models
+
+    // Model lịch sử mua hàng dùng hiển thị trong DataGrid
+    public class PurchaseHistory
+    {
+        public int STT { get; set; }
+        public string MaHD { get; set; }
+        public string TongTien { get; set; }
+        public string NgayMuaHang { get; set; }
+        public string GhiChu { get; set; }
+    }
+
+    #endregion
 }
--- bookstore_Management/bookstore_Management/Presentation/Views/Customers/CustomerListView.xaml.cs
+++ bookstore_Management/bookstore_Management/Presentation/Views/Customers/CustomerListView.xaml.cs
@@ -1,18 +1,7 @@
-﻿using bookstore_Management.Models;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace bookstore_Management.Views.Customers
 {
@@ -21,25 +10,46 @@ namespace bookstore_Management.Views.Customers
     /// </summary>
     public partial class CustomerListView : UserControl
     {
+        public ObservableCollection<Customer> Customers { get; set; }
+        public event EventHandler<Customer> CustomerSelected;
+
+        // Khởi tạo danh sách khách hàng mẫu
         public CustomerListView()
         {
             InitializeComponent();
             LoadSampleData();
-
         }
 
-        // Sua
-        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        // Xử lý click nút thêm khách hàng
+        private void btnAdd_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-
         }
 
-        // Xoa
-        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        // Xử lý click nút sửa trong từng dòng
+        private void btnEdit_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-
         }
-        public ObservableCollection<Customer> Customers { get; set; }
+
+        // Xử lý click nút xóa trong từng dòng
+        private void btnDelete_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        }
+
+        // Xử lý thay đổi lựa chọn khách hàng (dùng khi cần mở rộng)
+        private void dgCustomers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+
+        // Xử lý double-click trên một dòng để mở màn chi tiết khách hàng
+        private void dgCustomers_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (dgCustomers.SelectedItem is Customer selectedCustomer)
+            {
+                CustomerSelected?.Invoke(this, selectedCustomer);
+            }
+        }
+
+        // Tải dữ liệu khách hàng mẫu cho DataGrid
         private void LoadSampleData()
         {
             Customers = new ObservableCollection<Customer>
@@ -56,28 +66,12 @@ namespace bookstore_Management.Views.Customers
                 new Customer { STT = 10, MaKH = "KH109", TenKH = "Lê Minh Tuấn", HangThanhVien = "Vàng", SDT = "0912345678", DoanhThu = 1200000000 },
                 new Customer { STT = 11, MaKH = "KH110", TenKH = "Trần Văn Nam", HangThanhVien = "Bạc", SDT = "0923456789", DoanhThu = 800000000 },
                 new Customer { STT = 12, MaKH = "KH111", TenKH = "Nguyễn Thị Lan", HangThanhVien = "Đồng", SDT = "0934567890", DoanhThu = 450000000 },
-                new Customer { STT = 1, MaKH = "KH100", TenKH = "Trần Thị Hồng Thanh", HangThanhVien = "Kim Cương", SDT = "0593793875", DoanhThu = 2760000000 },
-                new Customer { STT = 2, MaKH = "KH101", TenKH = "Nguyễn Ái My", HangThanhVien = "Bạch Kim", SDT = "0861278318", DoanhThu = 500000000 },
-                new Customer { STT = 3, MaKH = "KH102", TenKH = "Phạm Hoàng Gia Hiển", HangThanhVien = "Vàng", SDT = "0826437621", DoanhThu = 1500000000 },
-                new Customer { STT = 4, MaKH = "KH103", TenKH = "Phạm Nguyên Gia Huy", HangThanhVien = "Kim Cương", SDT = "0782648645", DoanhThu = 700000000 },
-                new Customer { STT = 5, MaKH = "KH104", TenKH = "Nguyễn Khánh Vi", HangThanhVien = "Đồng", SDT = "0173288246", DoanhThu = 300000000 },
-                new Customer { STT = 6, MaKH = "KH105", TenKH = "Nguyễn Quang Nghĩa", HangThanhVien = "Bạc", SDT = "0861278318", DoanhThu = 2760000000 },
-                new Customer { STT = 7, MaKH = "KH106", TenKH = "Võ Nguyên Khoa", HangThanhVien = "Bạc", SDT = "0826437621", DoanhThu = 500000000 },
-                new Customer { STT = 8, MaKH = "KH107", TenKH = "Nguyễn Vũ Phúc", HangThanhVien = "Đồng", SDT = "0782648645", DoanhThu = 1500000000 },
-                new Customer { STT = 9, MaKH = "KH108", TenKH = "Nguyễn Ngọc Hân", HangThanhVien = "Bạch Kim", SDT = "0173288246", DoanhThu = 700000000 },
-                new Customer { STT = 10, MaKH = "KH109", TenKH = "Lê Minh Tuấn", HangThanhVien = "Vàng", SDT = "0912345678", DoanhThu = 1200000000 },
-                new Customer { STT = 11, MaKH = "KH110", TenKH = "Trần Văn Nam", HangThanhVien = "Bạc", SDT = "0923456789", DoanhThu = 800000000 },
-                new Customer { STT = 12, MaKH = "KH111", TenKH = "Nguyễn Thị Lan", HangThanhVien = "Đồng", SDT = "0934567890", DoanhThu = 450000000 }
             };
 
             dgCustomers.ItemsSource = Customers;
         }
-
-        private void btnAdd_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
+
     public class Customer
     {
         public int STT { get; set; }
