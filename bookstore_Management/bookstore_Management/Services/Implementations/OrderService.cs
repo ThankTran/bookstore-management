52e52ac6c6bf157c09864996e3b9802af9499409
hienpham0344 <hienpham0344@gmail.com>
2025-12-10 00:52:40 +0700

    fix typo

--- bookstore_Management/bookstore_Management/Services/Implementations/OrderService.cs
+++ bookstore_Management/bookstore_Management/Services/Implementations/OrderService.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using bookstore_Management.Core.Enums;
@@ -623,3 +623,4 @@ namespace bookstore_Management.Services.Implementations
         }
     }
 }
+*/
--- bookstore_Management/bookstore_Management/Services/Implementations/ReportService.cs
+++ bookstore_Management/bookstore_Management/Services/Implementations/ReportService.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using bookstore_Management.Core.Results;
@@ -508,4 +508,4 @@ namespace bookstore_Management.Services.Implementations
             }
         }
     }
-}
+} */
