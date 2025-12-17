0f2b48b04ae599f854b4f2e0cfd361904b5bc541
thanhtran <hongthanh3aqn@gmail.com>
2025-12-17 20:33:54 +0700

    create new payment folder

---
+++ bookstore_Management/bookstore_Management/Presentation/Views/Payment/PaymentView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using bookstore_Management.ViewModels;
+
+namespace bookstore_Management.Views.Orders
+{
+    /// <summary>
+    /// Interaction logic for PaymentView.xaml
+    /// </summary>
+    public partial class PaymentView : UserControl
+    {
+        public PaymentView()
+        {
+            InitializeComponent();
+            //DataContext = new PaymentViewModel();
+        }
+    }
+}
