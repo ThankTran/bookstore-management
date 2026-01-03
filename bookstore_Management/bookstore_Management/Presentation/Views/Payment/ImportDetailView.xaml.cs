591477fe7f2869edc38f889aed6a3957def105d8
thanhtran <hongthanh3aqn@gmail.com>
2026-01-04 05:19:57 +0700

    update view

--- bookstore_Management/bookstore_Management/Presentation/Views/Payment/ImportDetailView.xaml.cs
+++ bookstore_Management/bookstore_Management/Presentation/Views/Payment/ImportDetailView.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace bookstore_Management.Views.Orders
+namespace bookstore_Management.Presentation.Views.Payment
 {
     /// <summary>
-    /// Interaction logic for OrderDetailView.xaml
+    /// Interaction logic for ImportDetailView.xaml
     /// </summary>
-    public partial class OrderDetailView : UserControl
+    public partial class ImportDetailView : UserControl
     {
-        public OrderDetailView()
+        public ImportDetailView()
         {
             InitializeComponent();
         }
