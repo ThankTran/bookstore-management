aa5827005e3db9a0973b768fcfd5194b46e9e348
hienpham0344 <hienpham0344@gmail.com>
2026-01-07 13:40:17 +0700

    feat: add new feature constants

--- bookstore_Management/bookstore_Management/Core/Enums/FeatureConstant.cs
+++ bookstore_Management/bookstore_Management/Core/Enums/FeatureConstant.cs
@@ -3,13 +3,12 @@
     /// <summary>
     /// Các tên feature trong hệ thống
     /// </summary>
-    public static class FeatureConstants
+    public enum FeatureConstants
     {
-        public const string Book = "Book";
-        public const string Billing = "Billing";
-        public const string Stock = "Stock";
-        public const string Employee = "Employee";
-        public const string Customer = "Customer";
-        public const string Report = "Report";
+        /*
+        book = 1,
+        customer = 2,
+        dashboard = 3
+        */
     }
 }   
