ccae9c87ecbaaea1504aa295061e65bbf72fe59b
thanhtran <hongthanh3aqn@gmail.com>
2026-01-18 10:08:40 +0700

    coding export print staff

---
+++ bookstore_Management/bookstore_Management/Presentation/Views/Dialogs/Staffs/ExportExcelStaff.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace bookstore_Management.Presentation.Views.Dialogs.Staffs
+{
+    /// <summary>
+    /// Interaction logic for ExportExcelStaff.xaml
+    /// </summary>
+    public partial class ExportExcelStaff : Window
+    {
+        public ExportExcelStaff()
+        {
+            InitializeComponent();
+        }
+    }
+}
